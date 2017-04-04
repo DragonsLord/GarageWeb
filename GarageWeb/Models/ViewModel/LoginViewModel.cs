aab05a8dd39bc99513c28794f97325850c3ada2a
AlexTruba <straber@ukr.net>
2017-04-04 21:24:20 +0300

    Add admin login page

---
+++ GarageWeb/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GarageWeb.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Логін")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
