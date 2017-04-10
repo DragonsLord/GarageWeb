159cc215d5e3ceb12129a70b88a98791a286e7fa
AlexTruba <straber@ukr.net>
2017-04-10 20:43:37 +0300

    Google Authentication
    Google Authentication

--- GarageWeb/App_Start/Startup.cs
+++ GarageWeb/App_Start/Startup.cs
@@ -2,6 +2,9 @@
 using Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Google;
+using GarageWeb.Infrastructure;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartup(typeof(GarageWeb.Startup))]
 namespace GarageWeb
@@ -10,10 +13,21 @@ namespace GarageWeb
     {
         public void Configuration(IAppBuilder app)
         {
+
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = "ApplicationCookie",
                 LoginPath = new PathString("/Admin/Login"),
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "373441158521-on3q879mcd6q0iqfmb4l6n6pd6bjj2gb.apps.googleusercontent.com",
+                ClientSecret = "9dQtQs34jGqQlPRxgCtEgOfS"
             });
         }
     }
--- GarageWeb/Controllers/AdminController.cs
+++ GarageWeb/Controllers/AdminController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Mvc; 
 
 using GarageWeb.Infrastructure;
 
